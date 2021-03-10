ed354e8ba49b684f9cfebb4ac148c01b91474e92
tylerdaughetrty <tyler.daugherty@live.com>
2021-03-10 12:12:39 -0500

    * musashi.db: * LessonCounter.cs: * musashi.db-journal: * FolderProfile 1.pubxml:
    * appsettings.json: returing classes with curriculum working as
      expected... atm.

---
+++ Models/LessonCounter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QueensGambit.Models
+{
+    public class LessonCounter 
+    {
+        static public int class_count {get; set;}
+
+        public int class_id { get; set; }
+        public string curriculum { get; set; }
+        public string curriculum1 { get; set; }
+        public string curriculum2 { get; set; }
+
+    }
+}
